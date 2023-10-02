d6d9ade65438517277d323bfc97143a2319c80cc
s.suzuki <shunya_suzuki@post.pioneer.co.jp>
2023-10-02 23:17:32 +0900

    再度up

---
+++ src/ViewThread.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import MovePage from "./MovePage";
+import GetThreadTitle from "./GetThreadTitle";
+
+export const ViewThread = () => {
+  return (
+    <div>
+      <h3>新着スレッド</h3>
+      <button onClick={MovePage("/thread/new")}>新しいスレッドを作成</button>
+      {GetThreadTitle()}
+    </div>
+  );
+};
+export default ViewThread;
