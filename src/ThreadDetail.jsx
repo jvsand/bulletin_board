a084219711e4c34c5d067d3ed03da955d82a7888
s.suzuki <shunya_suzuki@post.pioneer.co.jp>
2023-10-04 22:03:47 +0900

    conflict

--- src/ThreadDetail.jsx
+++ src/ThreadDetail.jsx
@@ -1,10 +1,9 @@
-
 import React, { useEffect, useState } from "react";
 import MovePage from "./MovePage";
 import { useLocation } from "react-router-dom";
 import axios from "axios";
 
-export const ThreadDetail=()=> {
+export const ThreadDetail = () => {
   const location = useLocation();
   const { id, title } = location.state;
 
@@ -12,32 +11,31 @@ export const ThreadDetail=()=> {
   const threadDetailUrl = `https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads/${id}/posts?offset=0`;
   const postCommentUrl = `https://2y6i6tqn41.execute-api.ap-northeast-1.amazonaws.com/threads/${id}/posts`;
 
-
-  const handlePostComment=()=>{
+  const handlePostComment = () => {
     const newCommentContent = document.getElementById("newComment").value;
-  // API を使用して新しいコメントをサーバーに送信するロジックを追加
-  // 成功したらコメントデータを取得し、setDetailData で更新
-  // 失敗時のエラーハンドリングも追加
+    // API を使用して新しいコメントをサーバーに送信するロジックを追加
+    // 成功したらコメントデータを取得し、setDetailData で更新
+    // 失敗時のエラーハンドリングも追加
 
-  const postData = {
-    post: newCommentContent,
-  };
+    const postData = {
+      post: newCommentContent,
+    };
 
-  axios
-    .post(postCommentUrl, postData, {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-  .then((response) => {
-    // 成功したらコメントデータを更新
-    const newComment = response.data;
-    setDetailData((prevData) => [newComment, ...prevData]); // 最新のコメントを先頭に追加
-    console.log("投稿成功:", newComment);
-  })
-  .catch((error) => {
-    console.error("コメントの投稿エラー:", error);
-    });
+    axios
+      .post(postCommentUrl, postData, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      })
+      .then((response) => {
+        // 成功したらコメントデータを更新
+        const newComment = response.data;
+        setDetailData((prevData) => [newComment, ...prevData]); // 最新のコメントを先頭に追加
+        console.log("投稿成功:", newComment);
+      })
+      .catch((error) => {
+        console.error("コメントの投稿エラー:", error);
+      });
   };
 
   useEffect(() => {
@@ -58,17 +56,17 @@ export const ThreadDetail=()=> {
       <h1>title: {title}</h1>
       <p>id:{id}</p>
       <h3 className="">スレッドへの投稿</h3>
-       <input
-         id="newTitle"
-         // value={newTitle}
-         type="text"
-         size="50"
-         placeholder="内容を記載してください"
-         // onChange={handleChange}
-       ></input>
+      <input
+        id="newTitle"
+        // value={newTitle}
+        type="text"
+        size="50"
+        placeholder="内容を記載してください"
+        // onChange={handleChange}
+      ></input>
 
       {/* ここがまだできていない */}
-       {/* <table>
+      {/* <table>
          <tbody>
            {detailData.map((post) => (
           //  {detailData.slice(0,10).map((post) => (
@@ -80,13 +78,15 @@ export const ThreadDetail=()=> {
        </table> */}
 
       <div>
-        <button className="row-button" onClick={handlePostComment}>投稿</button>
+        <button className="row-button" onClick={handlePostComment}>
+          投稿
+        </button>
         <button className="row-button" onClick={MovePage("/")}>
-        戻る
+          戻る
         </button>
       </div>
     </div>
   );
-  }
+};
 
 export default ThreadDetail;
